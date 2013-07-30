cbbc45bb3e55b0116e0fe8ecb90cee24d8c82532
Ameya Thakur <ameyat@codeaurora.org>
2013-07-29 17:39:37 -0700

    build: Add changes to release tools and mkbootimg Change the prototype of LoadRecoveryFstab to take in the device type as a argument. Fix the case where mkbootimg was being passed a incorrect argument.
    Change-Id: Ic6ac596d8d96d3a5effbdf513763ec1cb92b1a03
    
    # Conflicts:
    #	tools/releasetools/common.py
    
    # Conflicts:
    #	core/Makefile
    #	tools/releasetools/common.py

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -205,6 +205,8 @@ def LoadInfoDict(input_file, input_dir=None):
             vendor_base_fs_file,))
         del d["vendor_base_fs_file"]
 
+  if "device_type" not in d:
+    d["device_type"] = "MMC"
   try:
     data = read_helper("META/imagesizes.txt")
     for line in data.split("\n"):
@@ -237,11 +239,11 @@ def LoadInfoDict(input_file, input_dir=None):
   system_root_image = d.get("system_root_image", None) == "true"
   if d.get("no_recovery", None) != "true":
     recovery_fstab_path = "RECOVERY/RAMDISK/etc/recovery.fstab"
-    d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"],
+    d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"], d["device_type"],
         recovery_fstab_path, system_root_image)
   elif d.get("recovery_as_boot", None) == "true":
     recovery_fstab_path = "BOOT/RAMDISK/etc/recovery.fstab"
-    d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"],
+    d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"], d["device_type"],
         recovery_fstab_path, system_root_image)
   else:
     d["fstab"] = None
@@ -271,7 +273,7 @@ def LoadDictionaryFromLines(lines):
   return d
 
 
-def LoadRecoveryFSTab(read_helper, fstab_version, recovery_fstab_path,
+def LoadRecoveryFSTab(read_helper, fstab_version, type, recovery_fstab_path,
                       system_root_image=False):
   class Partition(object):
     def __init__(self, mount_point, fs_type, device, length, context):
@@ -418,6 +420,11 @@ def _BuildBootableImage(sourcedir, fs_config_file, info_dict=None,
   fn = os.path.join(sourcedir, "tagsaddr")
   if os.access(fn, os.F_OK):
     cmd.append("--tags-addr")
+    cmd.append(open(fn).read().rstrip("\n"))
+
+  fn = os.path.join(sourcedir, "tags_offset")
+  if os.access(fn, os.F_OK):
+    cmd.append("--tags_offset")
     cmd.append(open(fn).read().rstrip("\n"))
 
   fn = os.path.join(sourcedir, "ramdisk_offset")
